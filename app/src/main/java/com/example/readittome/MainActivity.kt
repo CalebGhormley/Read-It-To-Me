3adcfd345b2b9474094ec08ab89af9a33fb23b19
Caleb Ghormley <calebghormley@yahoo.com>
2019-05-02 14:34:51 -0600

    Finished persistance requirement

--- app/src/main/java/com/example/readittome/MainActivity.kt
+++ app/src/main/java/com/example/readittome/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.example.readittome
 
 import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
 import android.content.IntentFilter
+import android.graphics.Color
 import android.media.AudioManager
 import android.net.wifi.p2p.WifiP2pManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.support.v7.app.AlertDialog
+import android.text.InputType
 import android.view.KeyEvent
+import android.widget.EditText
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -24,15 +29,21 @@ class MainActivity : AppCompatActivity() {
     private var tts_german: TTS? = null
     private var tts_spanish: TTS? = null
     public var tts_language: TTS? = null
-    public var textToBeSaid: String? = null
+    public var textToBeSaid: String = ""
+    private var list: ArrayList<TextObj> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        textToBeSaid = intent.getStringExtra("textToBeSaid")
-        if(textToBeSaid != null && textToBeSaid!!.isNotEmpty())textToSay.text.insert(0, textToBeSaid)
-
+        /*
+        if(intent != null) {
+            if (intent.getStringExtra("textToBeSaid") != null && textToBeSaid!!.isNotEmpty()) {
+                textToBeSaid = intent.getStringExtra("textToBeSaid")
+                textToSay.text.insert(0, textToBeSaid)
+            }
+        }
+        */
         /*
         // Indicates a change in the Wi-Fi P2P status.
         intentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION)
@@ -61,7 +72,46 @@ class MainActivity : AppCompatActivity() {
             tts_language!!.speak(textToSay.text.toString())
         }
 
+        saveButton.setOnClickListener{
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Save Text")
 
+            val input = EditText(this)
+            input.inputType = InputType.TYPE_CLASS_TEXT
+            builder.setView(input)
+
+            builder.setPositiveButton("Save") { dialog, which ->
+                var title = input.text.toString()
+                var newText: TextObj = TextObj()
+                newText.title = title
+                newText.text = textToSay.text.toString()
+
+                list.add(newText)
+
+                Toast.makeText(this,"current data " + title, Toast.LENGTH_LONG).show()
+            }
+
+            builder.setNegativeButton("Cancel") { dialog, _ -> dialog.cancel() }
+            builder.show()
+            true
+        }
+
+        loadButton.setOnClickListener{
+            var textArray = listToCharSeq(list)
+
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Select Text")
+            builder.setItems(textArray, DialogInterface.OnClickListener { dialog, i  ->
+
+                val selected = list[i]
+                textToSay.text.clear()
+                textToSay.text.insert(0,selected.text)
+
+                dialog.dismiss()
+            })
+            builder.show()
+            true
+        }
 
         fetchButton.setOnClickListener{
             val intent = Intent(this, FetchActivity::class.java)
@@ -83,6 +133,14 @@ class MainActivity : AppCompatActivity() {
             tts_german!!.speak(textToSay.text.toString())
         }
         */
+    }
+
+    private fun listToCharSeq(list: List<TextObj>): Array<CharSequence?> {
+        val myArray = arrayOfNulls<CharSequence>(list.size)
+        for (i in 0 until list.size) {
+            myArray[i] = list[i].title
+        }
+        return myArray
     }
 
     public override fun onDestroy() {
---
+++ app/src/main/java/com/example/readittome/TextObj.kt
@@ -0,0 +1,6 @@
+package com.example.readittome
+
+class TextObj {
+    public var title: String = ""
+    public var text: String = ""
+}
