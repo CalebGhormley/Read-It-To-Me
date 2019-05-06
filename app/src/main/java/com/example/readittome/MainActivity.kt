77bab266b7c41991f39c6e2256b2a91806510913
Caleb Ghormley <calebghormley@yahoo.com>
2019-05-05 20:48:51 -0600

    Finished import functionality

--- app/src/main/java/com/example/readittome/MainActivity.kt
+++ app/src/main/java/com/example/readittome/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.readittome
 
+import android.app.Activity
 import android.content.Context
 import android.content.DialogInterface
 import android.content.Intent
 import android.content.IntentFilter
+import android.database.Cursor
 import android.graphics.drawable.Icon
 import android.hardware.Sensor
 import android.hardware.SensorEvent
@@ -11,23 +13,34 @@ import android.hardware.SensorEventListener
 import android.media.AudioManager
 import android.net.ConnectivityManager
 import android.net.NetworkInfo
+import android.net.Uri
 import android.net.wifi.p2p.WifiP2pManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.provider.OpenableColumns
 import android.speech.tts.UtteranceProgressListener
 import android.speech.tts.Voice
 import android.support.v7.app.AlertDialog
 import android.text.InputType
+import android.util.Log
 import android.view.KeyEvent
 import android.view.View.INVISIBLE
 import android.view.View.VISIBLE
 import android.widget.EditText
 import android.widget.Toast
 import com.example.readittome.R.drawable.ic_pause
+import com.itextpdf.text.pdf.PdfReader
+import com.itextpdf.text.pdf.parser.PdfTextExtractor
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.net.URI
 import java.util.*
 import kotlin.collections.ArrayList
+
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
 
@@ -45,6 +58,8 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     private var playing: Boolean = false
     private var speed: Float = 1.0F
     private var size = 0
+    private val READ_REQUEST_CODE: Int = 42
+    private val WRITE_REQUEST_CODE: Int = 43
     private val mProgressListener = object : UtteranceProgressListener() {
         override fun onStart(utteranceId: String) {}
         override fun onError(utteranceId: String) {}
@@ -215,12 +230,12 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
             true
         }
 
-        sendButton.setOnClickListener{
+        exportButton.setOnClickListener{
 
         }
 
-        recieveButton.setOnClickListener{
-
+        importButton.setOnClickListener{
+            performFileSearch()
         }
 
         voiceButton.setOnClickListener{
@@ -402,5 +417,168 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    /**
+     * Fires an intent to spin up the "file chooser" UI and select an image.
+     */
+    fun performFileSearch() {
+
+        // ACTION_OPEN_DOCUMENT is the intent to choose a file via the system's file
+        // browser.
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+            // Filter to only show results that can be "opened", such as a
+            // file (as opposed to a list of contacts or timezones)
+            addCategory(Intent.CATEGORY_OPENABLE)
+
+            // Filter to show only images, using the image MIME data type.
+            // If one wanted to search for ogg vorbis files, the type would be "audio/ogg".
+            // To search for all documents available via installed storage providers,
+            // it would be "*/*".
+            type = "application/*"
+        }
+
+        startActivityForResult(intent, READ_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, resultData: Intent?) {
+
+        // The ACTION_OPEN_DOCUMENT intent was sent with the request code
+        // READ_REQUEST_CODE. If the request code seen here doesn't match, it's the
+        // response to some other intent, and the code below shouldn't run at all.
+
+        if (requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            // The document selected by the user won't be returned in the intent.
+            // Instead, a URI to that document will be contained in the return intent
+            // provided to this method as a parameter.
+            // Pull that URI using resultData.getData().
+            resultData?.data?.also { uri ->
+                //Log.i(TAG, "Uri: $uri")
+                textToSay.text.clear()
+                textToSay.text.insert(0,readTextFromUri(uri))
+            }
+        }
+    }
+
+    fun dumpImageMetaData(uri: Uri) {
+
+        // The query, since it only applies to a single document, will only return
+        // one row. There's no need to filter, sort, or select fields, since we want
+        // all fields for one document.
+        val cursor: Cursor? = contentResolver.query( uri, null, null, null, null, null)
+
+        cursor?.use {
+            // moveToFirst() returns false if the cursor has 0 rows.  Very handy for
+            // "if there's anything to look at, look at it" conditionals.
+            if (it.moveToFirst()) {
+
+                // Note it's called "Display Name".  This is
+                // provider-specific, and might not necessarily be the file name.
+                val displayName: String =
+                    it.getString(it.getColumnIndex(OpenableColumns.DISPLAY_NAME))
+                //Log.i(TAG, "Display Name: $displayName")
+
+                val sizeIndex: Int = it.getColumnIndex(OpenableColumns.SIZE)
+                // If the size is unknown, the value stored is null.  But since an
+                // int can't be null in Java, the behavior is implementation-specific,
+                // which is just a fancy term for "unpredictable".  So as
+                // a rule, check if it's null before assigning to an int.  This will
+                // happen often:  The storage API allows for remote files, whose
+                // size might not be locally known.
+                val size: String = if (!it.isNull(sizeIndex)) {
+                    // Technically the column stores an int, but cursor.getString()
+                    // will do the conversion automatically.
+                    it.getString(sizeIndex)
+                } else {
+                    "Unknown"
+                }
+                //Log.i(TAG, "Size: $size")
+            }
+        }
+    }
+
+    private fun createFile(mimeType: String, fileName: String) {
+        val intent = Intent(Intent.ACTION_CREATE_DOCUMENT).apply {
+            // Filter to only show results that can be "opened", such as
+            // a file (as opposed to a list of contacts or timezones).
+            addCategory(Intent.CATEGORY_OPENABLE)
+
+            // Create a file with the requested MIME type.
+            type = mimeType
+            putExtra(Intent.EXTRA_TITLE, fileName)
+        }
+
+        startActivityForResult(intent, WRITE_REQUEST_CODE)
+    }
+
+    @Throws(IOException::class)
+    private fun readTextFromUri(uri: Uri): String {
+        /*
+         * Get the file's content URI from the incoming Intent, then
+         * get the file's MIME type
+         */
+        val mimeType: String? = contentResolver.getType(uri)
+        var returnText: String = ""
+
+        if(mimeType!!.contains("pdf")){
+            val stringBuilder = StringBuilder()
+            contentResolver.openInputStream(uri)?.use { inputStream ->
+                returnText = pdfToText(inputStream)
+            }
+        }
+        else{
+            val stringBuilder = StringBuilder()
+            contentResolver.openInputStream(uri)?.use { inputStream ->
+                BufferedReader(InputStreamReader(inputStream)).use { reader ->
+                    var line: String? = reader.readLine()
+                    while (line != null) {
+                        stringBuilder.append(line)
+                        line = reader.readLine()
+                    }
+                }
+            }
+            returnText = stringBuilder.toString()
+        }
+        return returnText
+    }
+
+    @Throws(IOException::class)
+    private fun readText(uri: Uri): String {
+        val stringBuilder = StringBuilder()
+        contentResolver.openInputStream(uri)?.use { inputStream ->
+            BufferedReader(InputStreamReader(inputStream)).use { reader ->
+                var line: String? = reader.readLine()
+                while (line != null) {
+                    stringBuilder.append(line)
+                    line = reader.readLine()
+                }
+            }
+        }
+        return stringBuilder.toString()
+    }
+
+    private fun pdfToText(pdfPath: InputStream):String{
+        var returnText: String = ""
+        try {
+            var parsedText: String = ""
+            val reader: PdfReader = PdfReader(pdfPath)
+            var n: Int = reader.getNumberOfPages()
+            for (i in 0 until n) {
+                parsedText   = parsedText+ PdfTextExtractor.getTextFromPage(reader, i+1).trim()+" " //Extracting the content from the different pages
+            }
+            returnText = parsedText
+            reader.close()
+        } catch (e: Exception) {
+            System.out.println(e)
+        }
+        return returnText
+    }
+
+    private fun removeEndOfLineChars(text: String):String{
+        val eol: Char = 0x0A.toChar()
+        while(text.contains(eol)) {
+            text.replace(eol, ' ')
+        }
+        return text
     }
 }
